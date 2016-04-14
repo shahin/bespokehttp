3dcd5e261fca24b6af935aac68b2cb287dc2bcf7
Shahin Saneinejad <shahin.saneinejad@gmail.com>
2016-04-14 10:05:11 -0700

    Add test

--- tests/handler_test.py
+++ tests/handler_test.py
@@ -7,7 +7,7 @@ from bespokehttp.handler import HttpRequestHandler
 
 class HttpRequestHandlerTestCase(unittest.TestCase):
 
-    def test_responds_to_request_for_file(self):
+    def test_responds_200_to_request_for_file(self):
         tempdir = tempfile.mkdtemp(dir='./')
 
         with open(os.path.join(tempdir, 'resource.txt'), 'w') as resource_file:
@@ -20,6 +20,13 @@ class HttpRequestHandlerTestCase(unittest.TestCase):
         self.assertTrue(handler.wfile.read().startswith('HTTP/1.0 200 OK'))
 
         shutil.rmtree(tempdir)
+
+    def test_responds_404_to_request_for_file(self):
+        request = 'GET {} HTTP/1.0'.format('nonexistent')
+        handler = HttpRequestHandler(request)
+        handler.handle()
+        handler.wfile.seek(0)
+        self.assertTrue(handler.wfile.read().startswith('HTTP/1.0 404 Not Found'))
 
 if __name__ == '__main__':
     unittest.main()
